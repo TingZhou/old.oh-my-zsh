9bf1164a92047b52659e20bafc297e9128511b9e
Ting Zhou <hydrazt@gmail.com>
2013-03-16 14:52:23 +0000

    port some custem/* from lxzsh

---
+++ custom/aliases.zsh
@@ -0,0 +1,19 @@
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+alias ls='ls --color=auto'
+alias l='ls -l'
+alias ll='ls -ahlF'
+
+alias rm="rm -I"
+
+alias df='df -h'
+alias lg='ll | grep'
+alias pg='ps aux | grep'
+
+alias tmux='tmux -2'
+alias tmuxa='tmux a || tmux'
+
+alias rmds='find . -name ".DS_Store" -delete'
+alias rmpyc='find . -name "*.pyc" -delete'
---
+++ custom/env.zsh
@@ -0,0 +1,59 @@
+# basic env settings for OSX
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_TYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+export EDITOR=vim
+export CLICOLOR=1
+
+# fix gevent block bug
+export GEVENT_RESOLVER=block
+
+# make man colorful
+function man() {
+    env \
+        LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+        LESS_TERMCAP_md=$(printf "\e[1;31m") \
+        LESS_TERMCAP_me=$(printf "\e[0m") \
+        LESS_TERMCAP_se=$(printf "\e[0m") \
+        LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+        LESS_TERMCAP_ue=$(printf "\e[0m") \
+        LESS_TERMCAP_us=$(printf "\e[1;32m") \
+            man "$@"
+}
+
+# auto verify add config git user name and email
+function git_user_verify() {
+    if [[ -d ".git" ]]; then
+        if ! git config --get user.name 1>/dev/null; then
+            echo -n "Git user.name not configured, please enter your name: "
+            read name
+            if [[ -n $name ]]; then
+                git config user.name $name
+            else
+                echo "name empty, not configured."
+            fi
+        fi
+
+        if ! git config --get user.email 1>/dev/null; then
+            echo -n "Git user.email not configured, please enter your email: "
+            read email
+            if [[ -n $email ]]; then
+                git config user.email $email
+            else
+                echo "email empty, not configured."
+            fi
+        fi
+    fi
+}
+
+function cd() {
+    if builtin cd "$@"; then
+        autoenv_init
+        git_user_verify
+        return 0
+    else
+        return $?
+    fi
+}
