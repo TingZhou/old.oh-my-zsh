ed9fa6a73d4ab0dc7993aba883f63a6bfb568a1a
Ting Zhou <ting.zhou@ele.me>
2013-03-29 12:12:57 +0800

    ls -G for mac ls --color for linux

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -2,7 +2,13 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
-alias ls='ls --color=auto'
+if ls --color > /dev/null 2>&1; then
+    colorflag="--color"
+else # OS X `ls`
+    colorflag="-G"
+fi
+alias l="ls -l ${colorflag}"
+#alias ls='ls --color=auto'
 alias l='ls -l'
 alias ll='ls -ahlF'
 
